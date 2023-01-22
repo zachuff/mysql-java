b6c76854e4c08c801ce023b2b6dcfbd88b46b871
zacha <zachuff@yahoo.com>
2023-01-21 18:13:49 -0700

    added the menu application

---
+++ mysql-java/src/main/java/projects/service/ProjectService.java
@@ -0,0 +1,15 @@
+package projects.service;
+
+import projects.entity.Project;
+import projects.dao.ProjectDao;
+
+public class ProjectService {
+	
+	private ProjectDao projectDao = new ProjectDao();
+
+	public Project addProject(Project project) {
+		
+		return projectDao.insertProject(project);
+	}
+
+}
