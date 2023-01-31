60ec267790f91ca85e690988983f83c13e6afc20
zacha <zachuff@yahoo.com>
2023-01-31 05:37:49 -0700

    added an update and delete

--- mysql-java/src/main/java/projects/dao/ProjectDao.java
+++ mysql-java/src/main/java/projects/dao/ProjectDao.java
@@ -27,7 +27,8 @@ public class ProjectDao extends DaoBase
 	private static final String PROJECT_CATEGORY_TABLE = "project_category";
 	private static final String STEP_TABLE = "step";
 
-	public Project insertProject(Project project) {
+	public Project insertProject(Project project) 
+	{
 		
 		// @formatter:off
 		String sql = ""
@@ -219,6 +220,78 @@ public class ProjectDao extends DaoBase
 				}
 				return materials;
 			}
+		}
+	}
+
+	public boolean modifyProjectDetails(Project project) 
+	{
+		// @formatter:off
+		String sql = ""
+				+ "UPDATE " + PROJECT_TABLE + " SET "
+				+ "project_name = ?, "
+				+ "estimated_hours = ?, "
+				+ "actual_hours = ?, "
+				+ "difficulty = ?, "
+				+ "notes = ? "
+				+ "WHERE project_id = ?";
+		// @formatter:on
+		
+		try (Connection conn = DbConnection.getConnection()) 
+		{
+			startTransaction(conn);
+			
+			try(PreparedStatement stmt = conn.prepareStatement(sql)) 
+			{
+				setParameter(stmt, 1, project.getProjectName(), String.class);
+				setParameter(stmt, 2, project.getEstimatedHours(), BigDecimal.class);
+				setParameter(stmt, 3, project.getActualHours(), BigDecimal.class);
+				setParameter(stmt, 4, project.getDifficulty(), Integer.class);
+				setParameter(stmt, 5, project.getNotes(), String.class);
+				setParameter(stmt, 6, project.getProjectId(), Integer.class);
+				
+				boolean modified = stmt.executeUpdate() == 1;
+				commitTransaction(conn);
+				
+				return modified;
+			}
+			catch(Exception e) 
+			{
+				rollbackTransaction(conn);
+				throw new DbException(e);
+				
+			}
+		}
+		catch(SQLException e) 
+		{
+			throw new DbException(e);
+		}
+	}
+
+	public boolean deleteProject(Integer projectId) {
+		String sql = "DELETE FROM " + PROJECT_TABLE + " WHERE project_id = ?";
+		
+		try(Connection conn = DbConnection.getConnection()) 
+		{
+			startTransaction(conn);
+			
+			try(PreparedStatement stmt = conn.prepareStatement(sql))
+			{
+				setParameter(stmt, 1, projectId, Integer.class);
+				
+				boolean deleted = stmt.executeUpdate() == 1;
+				
+				commitTransaction(conn);
+				return deleted;
+			}
+			catch(Exception e)
+			{
+				rollbackTransaction(conn);
+				throw new DbException(e);
+			}
+		}
+		catch(SQLException e)
+		{
+			throw new DbException(e);
 		}
 	}
 
--- mysql-java/src/main/java/projects/service/ProjectService.java
+++ mysql-java/src/main/java/projects/service/ProjectService.java
@@ -1,6 +1,7 @@
 package projects.service;
 
 import projects.entity.Project;
+import projects.exception.DbException;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -8,23 +9,47 @@ import java.util.NoSuchElementException;
 
 import projects.dao.ProjectDao;
 
-public class ProjectService {
+public class ProjectService 
+{
 	
 	private ProjectDao projectDao = new ProjectDao();
 
-	public Project addProject(Project project) {
+	public Project addProject(Project project) 
+	{
 		
 		return projectDao.insertProject(project);
 	}
 
-	public List<Project> fetchAllProjects() {
+	public List<Project> fetchAllProjects() 
+	{
 		
 		return projectDao.fetchAllProjects();
 	}
 
-	public Project fetchProjectById(Integer projectId) {
+	public Project fetchProjectById(Integer projectId) 
+	{
 	
 		return projectDao.fetchProjectById(projectId).orElseThrow(() -> new NoSuchElementException("Project with project ID=" + projectId + " does not exist."));
 	}
+
+	public void modifyProjectDetails(Project project) 
+	{
+		
+		if(!projectDao.modifyProjectDetails(project)) 
+		{
+			throw new DbException("Project with ID=" + project.getProjectId() + " does not exist.");
+		}
+	}
+
+	public void deleteProject(Integer projectId) 
+	{
+		if(!projectDao.deleteProject(projectId)) 
+		{
+			throw new DbException("Project with ID=" + projectId + " does not exist.");
+		}
+		
+	}
+
+	
 
 }
